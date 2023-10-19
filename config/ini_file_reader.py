a6605344b168d644175826cd5a500178e046fc21
Iuliia603 <iuliia60358@gmail.com>
2023-10-19 18:15:40 -0400

    Create ini_file_reader.py

---
+++ config/ini_file_reader.py
@@ -0,0 +1,31 @@
+from configparser import ConfigParser
+
+class IniFileReader:
+    def __init__(self, filename):
+        self.data = None
+
+        with open(filename, 'r') as config_file:
+            self.data = ConfigParser()
+            self.data.read_file(config_file)
+
+    def get_browser(self):
+        value = self.data.get('environment', 'browser', fallback=None)
+        if value is None:
+            raise Exception("Browser option is not present in the config file")
+        return value
+
+    def get_wait_time(self):
+        value = self.data.get('environment', 'wait', fallback=None)
+        if value is None:
+            raise Exception("Wait_time option is not present in the config file")
+        return int(value)
+
+    def get_email(self, user_section):
+        if not self.data.has_option(user_section, 'email'):
+            raise Exception(f"Email option is not present in the {user_section} section of the config file")
+        return self.data.get(user_section, 'email')
+
+    def get_password(self, user_section):
+        if not self.data.has_option(user_section, 'password'):
+            raise Exception(f"Password option is not present in the {user_section} section of the config file")
+        return self.data.get(user_section, 'password')
